a2b5d151199cc25dcae058f27719e271428d68c5
abutler911 <abutler911@gmail.com>
2023-05-17 10:21:32 -0600

    register and auth is working

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const authRoutes = require("./routes/auth");
-
+// Import User model
+const User = require("./src/models/user");
 // Create Express app
 const app = express();
 
@@ -16,7 +21,6 @@ mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
@@ -24,15 +28,75 @@ mongoose
   .catch((error) => {
     console.error("MongoDB connection error:", error);
   });
+app.use(
+  session({
+    secret: "myfunapp",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
+
+// Configure Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define the local strategy
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username", // Assuming "username" is the unique identifier
+    },
+    (username, password, done) => {
+      // Find the user by username
+      User.findOne({ username: username })
+        .exec()
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: "Invalid username or password",
+            });
+          }
+          // Compare the password using bcrypt
+          bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+              return done(err);
+            }
+            if (!result) {
+              return done(null, false, {
+                message: "Invalid username or password",
+              });
+            }
+            return done(null, user);
+          });
+        })
+        .catch((err) => done(err));
+    }
+  )
+);
+
+// Serialize and deserialize user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => done(err));
+});
 
 // Configure middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/src/public"));
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.set("views", __dirname + "/src/views");
 
 // Set up Express Layouts
 app.use(expressLayouts);
@@ -44,7 +108,77 @@ app.get("/", (req, res) => {
   res.render("index", { layout: "layouts/layout" });
 });
 
-app.use("/api/auth", authRoutes);
+app.get("/register", (req, res) => {
+  res.render("register", { layout: "layouts/layout" });
+});
+
+app.post("/register", (req, res) => {
+  const { firstName, lastName, username, email, password } = req.body;
+
+  // Generate a salt with a desired number of rounds
+  const saltRounds = 10;
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) {
+      console.error("Error generating salt:", err);
+      return res.redirect("/register");
+    }
+
+    // Hash the password using the generated salt
+    bcrypt.hash(password, salt, (err, hashedPassword) => {
+      if (err) {
+        console.error("Error hashing password:", err);
+        return res.redirect("/register");
+      }
+
+      // Create a new user instance with the salted and hashed password
+      const newUser = new User({
+        firstName,
+        lastName,
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      // Save the user to the database
+      newUser
+        .save()
+        .then(() => {
+          console.log("User saved to the database");
+          req.flash("success", "User registered successfully!");
+          res.redirect("/login");
+        })
+        .catch((error) => {
+          console.error("Error saving user:", error);
+          req.flash("error", "Failed to register user.");
+          res.redirect("/register");
+        });
+    });
+  });
+});
+
+// Login route
+app.get("/login", (req, res) => {
+  res.render("login", {
+    layout: "layouts/layout",
+    message: req.flash("error"),
+  });
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/dashboard", (req, res) => {
+  const user = req.user;
+  const username = user.username;
+
+  res.render("dashboard", { username });
+});
 
 // Start the server
 app.listen(PORT, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const characterSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  strength: { type: Number, required: true },
-  stamina: { type: Number, required: true },
-  // Will add more
-});
-
-// User schema
 const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  character: characterSchema,
 });
 
-// Create models
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
